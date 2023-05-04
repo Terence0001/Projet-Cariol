44a454a7c78c0e59e33887963def967724fa4cbb
lucas <lucas.lvn97439@gmail.com>
2023-05-04 10:39:09 +0400

    Ajout du composant filters

---
+++ front/src/components/Filters.vue
@@ -0,0 +1,21 @@
+<script>
+    export default (await import('vue')).defineComponent({
+        data(){
+            return {
+
+            }
+        },
+        
+        methods: {
+            loadFilters: function(){
+                    
+            }
+        }
+    })
+</script>
+
+<template>
+    <section id="filters-bar">
+        
+    </section>
+</template>
