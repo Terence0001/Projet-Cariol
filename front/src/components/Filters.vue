2e9699f5b9dbfbb1fc97c9cf57c127ff48a2019b
lucas <lucas.lvn97439@gmail.com>
2023-05-04 11:36:18 +0400

    Ajout du filtre couleur

--- front/src/components/Filters.vue
+++ front/src/components/Filters.vue
@@ -1,14 +1,22 @@
 <script>
+    import {request} from '../services/request.js'
+
     export default (await import('vue')).defineComponent({
         data(){
             return {
+                colors: [
+                    {color: 'Couleur', id: 0},
+                    {color: 'rouge', id: 1},
+                    {color: 'bleu', id: 2},
 
+                ],
+                selectedColor: 0
             }
         },
         
         methods: {
             loadFilters: function(){
-                    
+
             }
         }
     })
@@ -16,6 +24,14 @@
 
 <template>
     <section id="filters-bar">
-        
+        <h3>Définiser les caractériques de votre voiture</h3>
+
+        <div>
+            <select v-model="selectedColor" >
+                <option value="none" selected>Couleur</option>
+                <option v-for="color in colors" :key="color" :value=color.id>{{ color.color }}</option>
+            </select>
+        </div>
+
     </section>
 </template>
