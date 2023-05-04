e0a06b7b083a8b518d53a8dfd2d57727f8031287
lucas <lucas.lvn97439@gmail.com>
2023-05-04 11:35:33 +0400

    Vuetify retirer, pas d'utiliter ici

--- front/src/main.js
+++ front/src/main.js
@@ -5,12 +5,6 @@ import App from './App.vue'
 import 'vuetify/styles'
 import './assets/main.css'
 
-//VUETIFY COMPONENTS
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
-createApp(App).use(vuetify).mount('#app')
+
+createApp(App).mount('#app')
