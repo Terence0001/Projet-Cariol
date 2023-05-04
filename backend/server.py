f3d9f79850285120f305b01de4372b5c972033fe
lucas <lucas.lvn97439@gmail.com>
2023-05-04 11:36:53 +0400

    Ajout d'une url + méthode pour récupérer les options de filtre (à finir)

--- backend/server.py
+++ backend/server.py
@@ -3,3 +3,20 @@ import hug
 
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
+
+
+@hug.get('/api/load-filters')
+def loadFilters(body):
+    return {
+        "colors":       ['rouge'],
+        "cylinders":    [6.0],
+        "airbags":      [1],
+        "doors":        [1, 2, 3, 4, 5],
+        "categories":   ['jeep'],
+        "driveWheel":   ['front'],
+        "gearBoxType":  ['automatic'],
+        "model":        ['prius'],
+        "manufacturer": ['toyota'],
+        "engineVolume": [1.8],
+        "fuelType":     ["hybrided"],
+    }
