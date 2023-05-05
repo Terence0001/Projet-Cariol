0d8531cab619094046b6628fc57a2dc11f313642
lucas <lucas.lvn97439@gmail.com>
2023-05-05 11:27:22 +0400

    Ajout des state filtres

--- front/src/components/Filters.vue
+++ front/src/components/Filters.vue
@@ -10,14 +10,55 @@
                     {color: 'bleu', id: 2},
 
                 ],
-                selectedColor: 0
+                selectedColor: 0,
+
+                models: [],
+                selectedModel: 0,
+
+                manufacturers: [],
+                manufacturersSelected: 0,
+
+                categories: [],
+                manufacturersSelected: 0,
+
+                fueltype: [],
+                fueltypeSelected: 0,
+
+                engineVolume: [],
+                engineVolumeSelected: 0,
+
+                NbDoors: [],
+                NbDoorsSelected: 0,
+
+                driveWheel: [],
+                driveWheelSelected: 0,
+
+                gearBox: [],
+                gearBoxSelected: 0,
+
+                NbAibags: [],
+                NbAibagsSelected: 0,
+
+                cylinders: [],
+                cylindersSelected: 0
             }
         },
         
-        methods: {
-            loadFilters: function(){
+        mounted(){
+            this.loadFilters()
+        },
 
-            }
+        methods: {
+            loadFilters: async function(){
+                const response = await request('load-filters', 'GET', null)
+                const json     = await response.json()
+
+                this.initFilters(json)
+            },
+
+            initFilters: function(filters){
+                this.colors = filters.colors
+            }   
         }
     })
 </script>
@@ -27,10 +68,15 @@
         <h3>Définiser les caractériques de votre voiture</h3>
 
         <div>
+
+            <select v-model="selectedModel" >
+                <option v-for="model in models" :key="model" :value=model.id>{{ model.color }}</option>
+            </select>
+
             <select v-model="selectedColor" >
-                <option value="none" selected>Couleur</option>
                 <option v-for="color in colors" :key="color" :value=color.id>{{ color.color }}</option>
             </select>
+
         </div>
 
     </section>
