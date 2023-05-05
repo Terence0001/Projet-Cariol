26e911248a21bd59ed21faa642d061d16351b2c7
lucas <lucas.lvn97439@gmail.com>
2023-05-05 11:27:02 +0400

    Retour des filtres

--- backend/server.py
+++ backend/server.py
@@ -8,8 +8,19 @@ api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origin
 @hug.get('/api/load-filters')
 def loadFilters(body):
     return {
-        "colors":       ['rouge'],
-        "cylinders":    [6.0],
+        "colors":[
+            {"id": 0, "color": 'Couleur'},
+            {"id": 1, "color": 'rouge'},
+            {"id": 2, "color": 'bleu'},
+            {"id": 3, "color": 'vert'}
+        ],
+
+        "cylinders":    [
+            {"id": 1, "cylinder": 6.0},
+            {"id": 2, "cylinder": 7.0},
+            {"id": 3, "cylinder": 8.0}
+
+        ],
         "airbags":      [1],
         "doors":        [1, 2, 3, 4, 5],
         "categories":   ['jeep'],
