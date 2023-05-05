d814ca571a3a4b055dd4ebf39875e2a20b2e1a8f
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-05-05 15:08:51 +0400

    Ajout du modèle manquant FuelType

---
+++ backend/database/models/FuelType.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Column, Integer, String
+
+try:
+    from database import Base
+except:
+    from ..database import Base
+
+
+class FuelType(Base):
+    __tablename__ = 'fuel_type'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    fuel = Column(String)
