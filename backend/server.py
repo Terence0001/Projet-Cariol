dd09139690406b670c4ab7c51a178af7c5c0db1f
lucas <lucas.lvn97439@gmail.com>
2023-05-11 19:22:02 +0400

    Récupération des données de filtrages + retour sous format json de c'est données

--- backend/server.py
+++ backend/server.py
@@ -1,94 +1,84 @@
 from hug.middleware import CORSMiddleware
 import hug
 from database.database import session
-from database.models import Airbags
-from database.Serializers import AirbagsSerializer
+from database.models.Colors import Colors
+from database.models.Cylinders import Cylinders
+from database.models.Categories import Categories 
+from database.models.Doors import Doors 
+from database.models.Airbags import Airbags
+from database.models.DriveWheel import DriveWheel
+from database.models.GearBoxType import GearBoxType
+from database.models.Model import Model
+from database.models.Manufacturer import Manufacturer
+from database.models.EngineVolume import EngineVolume
+from database.models.FuelType import FuelType
+
+
+# from database.Serializers import AirbagsSerializer
 
 
 api = hug.API(__name__)
 # allow_origins à restreindre pour le déploiement
 api.http.add_middleware(CORSMiddleware(api, allow_origins=['*']))
 
+''' Ajout une valeur avant toutes les autres dans la list,
+    la valeur ajouter et une valeur par défaut pour l'élément sélect
+    { id: 0, 'categorie': 'Catégorie'}
+'''
+def appendDefaultValue(featureName, featureValue, rows):
+    featureJson = [{'id': 0, featureName: featureValue}]
+    for row in rows:
+        featureJson.append(row.to_dict())
+    return featureJson
 
 @hug.get('/api/load-filters')
 def loadFilters(body):
+    colors = session.query(Colors).all()
+    colors = appendDefaultValue('color', 'Couleur', colors)
+
+    cylinders = session.query(Cylinders).all()
+    cylinders = appendDefaultValue('cylinders', 'Cylinder', cylinders) 
+    
+    categories = session.query(Categories).all()
+    categories = appendDefaultValue('categorie', 'Categorie', categories) 
+    
+    doors = session.query(Doors).all()
+    doors = appendDefaultValue('doors', 'Doors', doors) 
+
+    airbags = session.query(Airbags).all()
+    airbags = appendDefaultValue('airbags', 'Airbags', airbags)
+
+    drive_wheels = session.query(DriveWheel).all()
+    drive_wheels = appendDefaultValue('drive_wheels', 'Drive Wheel', drive_wheels)
+
+    gear_box = session.query(GearBoxType).all()
+    gear_box = appendDefaultValue('gears_type', 'Boîte de vitesse', gear_box)
+
+    models = session.query(Model).all()
+    models = appendDefaultValue('model', 'Modèles', models)
+
+    manufacturers = session.query(Manufacturer).all()
+    manufacturers = appendDefaultValue('manufacturers', 'Marque', manufacturers)
+    
+    engines_volume = session.query(EngineVolume).all()
+    engines_volume = appendDefaultValue('volume', 'Volume moteur', engines_volume)
+
+    fuels_type = session.query(FuelType).all()
+    fuels_type = appendDefaultValue('fuel', 'Carburant', fuels_type)
+
     return {
-        "colors":[
-            {"id": 0, "color": 'Couleur'},
-            {"id": 1, "color": 'rouge'},
-            {"id": 2, "color": 'bleu'},
-            {"id": 3, "color": 'vert'}
-        ],
-
-        "cylinders":    [
-            {"id": 0, "nb_cylinder": "Nb cylindres"},
-            {"id": 1, "nb_cylinder": 6.0},
-            {"id": 2, "nb_cylinder": 7.0},
-            {"id": 3, "nb_cylinder": 8.0}
-
-        ],
-        "airbags":      [
-            {"id": 0, 'nb_airbag': 'Nb airbagues'},
-            {"id": 1, 'nb_airbag': 1},
-        ],
-
-        "doors":        [
-            {"id": 0, 'nb_door': 'Nb portes'},
-            {"id": 1, 'nb_door': 1},
-            {"id": 2, 'nb_door': 2},
-
-
-        ],
-
-        "categories":   [
-            {"id": 0, 'categorie': 'Categorie'},
-            {"id": 1, 'categorie': 'Jeep'},
-        ],
-        
-        "drive_wheels":   [
-            {"id": 0, 'type': 'Drive Wheel'},
-            {"id": 1, 'type': '...'},
-        ],
-        
-        "gears_type":  [
-            {"id": 0, 'type': 'Gear Box Type'},
-            {"id": 1, 'type': 'Automatique'},
-        ],
-        
-        "models":        [
-            {"id": 0, "model": 'Modèles'},
-            {"id": 1, "model": 'x9'}
-        
-        ],
-
-        "manufacturers": [
-            {"id": 0, "name": 'Constructeur'},
-            {"id": 1, "name": 'Toyota'}
-
-        ],
-        
-        "engines_volume": [
-            {"id": 0, "engine_volume": 'Volume moteur'},
-            {"id": 1, "engine_volume": 6.0}
-
-
-        ],
-
-        "fuels_type": [
-            {"id": 0, "fuel": 'Type de carburant'}
-
-        ],
+        "engines_volume": engines_volume,
+        "drive_wheels":  drive_wheels,
+        "manufacturers":manufacturers,
+        "categories": categories,
+        "gears_type":  gear_box,
+        "cylinders": cylinders,
+        "colors":    colors,
+        "airbags":airbags,
+        "models": models,
+        "doors": doors,
+        "fuels_type": fuels_type,
     }
-
-# Fuck sa marche pas
-
-
-
-@hug.get('/api/testSerializer')
-def TestSerializer(body):
-    query = AirbagsSerializer.all()
-    serializer = query.to_dict()
-
 
 import time
 @hug.post('/api/prediction')
