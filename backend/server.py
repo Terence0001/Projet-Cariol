cd72186f25bfff6885b25804a2113b48e3eecceb
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-06-06 21:23:57 +0400

    Ajout de méthode + url API
    J'ai ajouté le code de pascal dans une méthode nommée def prediction_with_csv():, que j'importe dans le fichier servers.py et que j'utilise dans une autre méthode nomméé def prediction_with_local_data():

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,12 @@
+import time
 from hug.middleware import CORSMiddleware
 import hug
+from database.Car_Price_Prediction import prediction_with_csv
 from database.database import session
 from database.models.Colors import Colors
 from database.models.Cylinders import Cylinders
-from database.models.Categories import Categories 
-from database.models.Doors import Doors 
+from database.models.Categories import Categories
+from database.models.Doors import Doors
 from database.models.Airbags import Airbags
 from database.models.DriveWheel import DriveWheel
 from database.models.GearBoxType import GearBoxType
@@ -25,11 +27,14 @@ api.http.add_middleware(CORSMiddleware(api, allow_origins=['*']))
     la valeur ajouter et une valeur par défaut pour l'élément sélect
     { id: 0, 'categorie': 'Catégorie'}
 '''
+
+
 def appendDefaultValue(featureName, featureValue, rows):
     featureJson = [{'id': 0, featureName: featureValue}]
     for row in rows:
         featureJson.append(row.to_dict())
     return featureJson
+
 
 @hug.get('/api/load-filters')
 def loadFilters(body):
@@ -37,19 +42,20 @@ def loadFilters(body):
     colors = appendDefaultValue('color', 'Couleur', colors)
 
     cylinders = session.query(Cylinders).all()
-    cylinders = appendDefaultValue('cylinders', 'Cylinder', cylinders) 
-    
+    cylinders = appendDefaultValue('cylinders', 'Cylinder', cylinders)
+
     categories = session.query(Categories).all()
-    categories = appendDefaultValue('categorie', 'Categorie', categories) 
-    
+    categories = appendDefaultValue('categorie', 'Categorie', categories)
+
     doors = session.query(Doors).all()
-    doors = appendDefaultValue('doors', 'Doors', doors) 
+    doors = appendDefaultValue('doors', 'Doors', doors)
 
     airbags = session.query(Airbags).all()
     airbags = appendDefaultValue('airbags', 'Airbags', airbags)
 
     drive_wheels = session.query(DriveWheel).all()
-    drive_wheels = appendDefaultValue('drive_wheels', 'Drive Wheel', drive_wheels)
+    drive_wheels = appendDefaultValue(
+        'drive_wheels', 'Drive Wheel', drive_wheels)
 
     gear_box = session.query(GearBoxType).all()
     gear_box = appendDefaultValue('gears_type', 'Boîte de vitesse', gear_box)
@@ -58,10 +64,12 @@ def loadFilters(body):
     models = appendDefaultValue('model', 'Modèles', models)
 
     manufacturers = session.query(Manufacturer).all()
-    manufacturers = appendDefaultValue('manufacturers', 'Marque', manufacturers)
-    
+    manufacturers = appendDefaultValue(
+        'manufacturers', 'Marque', manufacturers)
+
     engines_volume = session.query(EngineVolume).all()
-    engines_volume = appendDefaultValue('volume', 'Volume moteur', engines_volume)
+    engines_volume = appendDefaultValue(
+        'volume', 'Volume moteur', engines_volume)
 
     fuels_type = session.query(FuelType).all()
     fuels_type = appendDefaultValue('fuel', 'Carburant', fuels_type)
@@ -69,20 +77,29 @@ def loadFilters(body):
     return {
         "engines_volume": engines_volume,
         "drive_wheels":  drive_wheels,
-        "manufacturers":manufacturers,
+        "manufacturers": manufacturers,
         "categories": categories,
         "gears_type":  gear_box,
         "cylinders": cylinders,
         "colors":    colors,
-        "airbags":airbags,
+        "airbags": airbags,
         "models": models,
         "doors": doors,
         "fuels_type": fuels_type,
     }
 
-import time
+
 @hug.post('/api/prediction')
 def prediction(body):
     print(body)
     time.sleep(1)
     return 'Le prix de la voiture vaut aujourd\'hui'
+
+
+@hug.get('/api/car/prediction')
+def prediction_with_local_data(body):
+    # Appel à la fonction prediction_with_csv() pour effectuer le traitement des données CSV
+    # et obtenir le résultat de la prédiction du prix des voitures
+    result = prediction_with_csv()
+    # Retourne le résultat de la prédiction en tant que réponse à la requête GET
+    return result
